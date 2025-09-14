e75062e90ad1458def0684fe5014a4015cd51d89
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-09-14 17:36:17 +0500

    Task5

---
+++ Tyuiu.HaevGS.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.HaevGS.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.HaevGS.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckerAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+
+        [TestMethod]
+        public void CheckerSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+
+
+        [TestMethod]
+        public void CheckerMultiplicationValid()
+        {
+            Assert.AreEqual(25, DataService.Multiplication(5, 5));
+        }
+
+
+        [TestMethod]
+        public void CheckerDivisionnValid()
+        {
+            Assert.AreEqual(2, DataService.Division(10, 5));
+        }
+    }
+}
---
+++ Tyuiu.HaevGS.Sprint0.Task5.V0/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.HaevGS.Sprint0.Task5.V0
+{
+    public class Class1
+    {
+    }
+}
