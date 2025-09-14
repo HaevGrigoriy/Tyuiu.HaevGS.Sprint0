0654e35072864689db58e7a59a4b031cde88da7d
Haev Grigoriy <tyuiu.haevgs@outlook.com>
2025-09-14 14:34:31 +0500

    Task2

---
+++ Tyuiu.HaevGS.Sprint0.Task2.V0.Test/DataServiceTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.HaevGS.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.HaevGS.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest1
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Григорий";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Григорий", res);
+        }
+    }
+}
